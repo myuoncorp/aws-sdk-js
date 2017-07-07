7610e9bbdf7031f9a5ed025c1daca4ce3ddb9124
YukiYonekura <yuki.yonekura@myu-on.co.jp>
2017-07-07 21:53:00 +0900

    add types (NodeHttpClient, Signers)

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,23 @@
-import {GlobalConfigInstance} from './lib/config';
+import { ServerResponse, ClientRequest } from 'https'
+import { GlobalConfigInstance } from './lib/config';
 
 export * from './lib/core';
 export * from './clients/all';
 export var config: GlobalConfigInstance
+
+export class NodeHttpClient {
+	handleRequest(
+		httpRequest: AWS.HttpRequest,
+		httpOptions: any,
+		callback: (res: ServerResponse) => void,
+		errCallback: (err: Error) => void): ClientRequest
+}
+
+export namespace Signers {
+	export class V4 {
+		constructor(req: AWS.HttpRequest, serviceName: string)
+		addAuthorization(creds: AWS.EnvironmentCredentials, date: Date): void
+	}
+}
 
 export as namespace AWS;
